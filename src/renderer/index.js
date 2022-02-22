9342e82c0e5d7a3849642e6ce0b81dade324653d
christides11 <29593332+christides11@users.noreply.github.com>
2022-02-22 17:25:10 -0500

    Setup device discovery page.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import LoginPage from '../main/login/login';
+import DiscoveryPage from '../main/discovery/discovery';
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path="/discovery" element={<DiscoveryPage />} />
       <Route
         path="*"
         element={
