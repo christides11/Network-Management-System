737323009a7a54defa30e4eb231aa9b7065aae9f
christides11 <29593332+christides11@users.noreply.github.com>
2022-02-22 17:56:49 -0500

    Function signatures for probe registration

--- Server/core_server.py
+++ Server/core_server.py
@@ -6,6 +6,29 @@ import scapy.all as scapy
 from mac_vendor_lookup import MacLookup
 from threading import Thread
 
+probes = []
+
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+async def TryRegisterProbe(sid, probeName, probeIP):
+    print("Trying to register probe {}.".format(probeIP))
+    probes.append({nickname: probeName, ip: probeIP, hid: "dummy"})
+
+@sio.event
+async def TryRemoveProbeByIP(sid, probeIP):
+    print("Trying to remove probe {}.".format(probbeIP))
+
+@sio.event
+async def TryRemoveProbeByName(sid, probeName):
+    print("Trying to remove probe {}.".format(probeName))
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
 if __name__ == '__main__':
     print("Run.")
     #web.run_app(app, host='localhost', port=8080)
