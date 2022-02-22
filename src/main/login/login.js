7bc982f41c3db55bf6622ba1a2a89d606d610f6a
christides11 <29593332+christides11@users.noreply.github.com>
2022-02-22 17:22:51 -0500

    Setup routing & login page.

---
+++ src/main/login/login.js
@@ -0,0 +1,11 @@
+import './login.css';
+
+function LoginPage(){
+    return (
+        <div className="LoginPage">
+            <h1>Login Page</h1>
+        </div>
+    );
+}
+
+export default LoginPage;
