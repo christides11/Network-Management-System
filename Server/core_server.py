130cde5c2a0565cb986961a9d84487cf96553f37
christides11 <29593332+christides11@users.noreply.github.com>
2022-02-22 19:49:31 -0500

    Option to pass IP & port args to the core server.

--- Server/core_server.py
+++ Server/core_server.py
@@ -5,6 +5,11 @@ import socketio
 import scapy.all as scapy
 from mac_vendor_lookup import MacLookup
 from threading import Thread
+import sys
+
+sio = socketio.AsyncServer(cors_allowed_origins='*')
+app = web.Application()
+sio.attach(app)
 
 probes = []
 
@@ -30,5 +35,10 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__ == '__main__':
-    print("Run.")
-    #web.run_app(app, host='localhost', port=8080)
+    serverIP = 'localhost'
+    serverPort = 8080
+    if len(sys.argv) > 1:
+        serverIP = str(sys.argv[1])
+    if len(sys.argv) > 2:
+        serverPort = int(sys.argv[2])
+    web.run_app(app, host=serverIP, port=serverPort)
