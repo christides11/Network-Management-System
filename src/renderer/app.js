6ddaa85eaa65387f7b6ff6d72b7972cbf690fa73
christides11 <29593332+christides11@users.noreply.github.com>
2022-02-23 20:28:50 -0500

    Created app.js

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,11 +1,31 @@
 import { io } from 'socket.io-client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../main/login/login';
+import DiscoveryPage from '../main/discovery/discovery';
+import DevicesPage from '../main/devices/devices';
+import DevicePage from '../main/device/device';
+import SummaryPage from '../main/summary/summary';
 
 const socket = io('http://localhost:8080');
 
 export default function App(){
     return (
-        <div className="App">
-            <h1>App</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/summary" element={<SummaryPage />} />
+                <Route path="/devices" element={<DevicesPage />} />
+                <Route path="/discovery" element={<DiscoveryPage />} />
+                <Route path="/device" element={<DevicePage />} />
+                <Route
+                    path="*"
+                    element={
+                    <main style={{ padding: "1rem" }}>
+                        <p>There's nothing here!</p>
+                    </main>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import LoginPage from '../main/login/login';
-import DiscoveryPage from '../main/discovery/discovery';
-import DevicesPage from '../main/devices/devices';
-import DevicePage from '../main/device/device';
-import SummaryPage from '../main/summary/summary';
 import App from './app';
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/summary" element={<SummaryPage />} />
-        <Route path="/devices" element={<DevicesPage />} />
-        <Route path="/discovery" element={<DiscoveryPage />} />
-        <Route path="/device" element={<DevicePage />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <App />,
   rootElement
 );
