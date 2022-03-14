e56da5aaaf185e5e45dba92b65d5ce172a0687ca
christides11 <29593332+christides11@users.noreply.github.com>
2022-03-14 13:03:05 -0400

    Get discovery jobs list from frontend.

--- src/main/discoveryjobs/discoveryjobs.js
+++ src/main/discoveryjobs/discoveryjobs.js
@@ -5,17 +5,34 @@ import { useNavigate } from 'react-router-dom';
 
 function DiscoveryJobsPage({socket}){
 
+    const [discoveryJobs, setDiscoveryJobs] = useState(null);
+
+    const receiveDiscoveryJobs = useCallback((data) => {
+        console.log(data);
+        setDiscoveryJobs(data);
+      }, []);
+
     useEffect(() => {
-        //socket.on("Frontend_RegisterDiscoveryScanResult", handleRegisterScanResult)
+        socket.on("ReceiveRegisteredDiscoveryScans", receiveDiscoveryJobs)
 
         return () => {
-            //socket.off("Frontend_RegisterDiscoveryScanResult", handleRegisterScanResult)
+            socket.off("ReceiveRegisteredDiscoveryScans", receiveDiscoveryJobs)
         }
     }, [socket]);
+
+    function RequestScanList(){
+        socket.emit('RequestRegisteredDiscoveryScans');
+    }
 
     return (
         <div className="DiscoveryJobsPage">
             <h1>Discovery Jobs Page</h1>
+            <button onClick={RequestScanList}>Request Scan List</button>
+            {discoveryJobs != null &&
+                <nav>
+                    <h2>Test.</h2>
+                </nav>
+            }
         </div>
     );
 }
