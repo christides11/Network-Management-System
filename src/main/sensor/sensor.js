4723a28f1f139c2697c4d4eee90e2835808f4f73
christides11 <29593332+christides11@users.noreply.github.com>
2022-04-02 15:42:45 -0400

    sensor page.

---
+++ src/main/sensor/sensor.js
@@ -0,0 +1,14 @@
+import './sensor.css';
+import { useParams } from "react-router-dom";
+
+function SensorPage(){
+    let params = useParams();
+    
+    return (
+        <div className="SensorPage">
+            <h1>Sensor ???</h1>
+        </div>
+    );
+}
+
+export default DiscoveryPage;
